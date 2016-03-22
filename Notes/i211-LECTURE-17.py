aa3205b122b1d7645904c49ecd3cf69632d94f0a
Liam Bolling <liam.bolling@gmail.com>
2016-03-22 10:33:22 -0400

    more crap

--- Notes/i211-LECTURE-17.py
+++ Notes/i211-LECTURE-17.py
@@ -27,45 +27,74 @@
 # out.close()
 #
 # webbrowser.open("template2.html")
+#
+# data = [["Item", "Cost", "Type"], ["Coke", "$2", "Drink"],
+#         ["Water", "$0", "Drink"], ["Fries", "$4", "Appetizer"],
+#         ["Onion Rings", "$3", "Appetizer"], ["Steak", "$12", "Entree"],
+#         ["Chicken", "$8", "Entree"], ["Caesar Salad", "$7", "Entree"]]
+#
+# out = open("template2.html","w")
+#
+# html = """
+# <html>
+#     <head>
+#         <title>Title</title>
+#     </head>
+#     <body>
+#         <h1>Hello</h1>
+#
+#
+#         <table style="width:100%;" border="1">
+# """
+#
+#
+#
+# for element in data:
+#     html+= """
+#         <tr>
+#         <td>"""+element[0]+"""</td>
+#         <td>"""+element[1]+"""</td>
+#         <td>"""+element[2]+"""</td>
+#         </tr>
+#     """
+#
+#
+#
+# html += """
+#         </table>
+#     </body>
+# </html>
+# """
+#
+# out.write(html)
+# out.close()
 
-data = [["Item", "Cost", "Type"], ["Coke", "$2", "Drink"],
-        ["Water", "$0", "Drink"], ["Fries", "$4", "Appetizer"],
-        ["Onion Rings", "$3", "Appetizer"], ["Steak", "$12", "Entree"],
-        ["Chicken", "$8", "Entree"], ["Caesar Salad", "$7", "Entree"]]
+import re
+import urllib.request, random
 
-out = open("template2.html","w")
+#out = open("template2.html","r+")
+#print(out)
 
-html = """
-<html>
-    <head>
-        <title>Title</title>
-    </head>
-    <body>
-        <h1>Hello</h1>
+webpage = urllib.request.urlopen("http://bloomington.in.gov/sections/viewSection.php?section_id=170")
+outLines = webpage.read().decode(errors="replace")
 
+# outLines = out.read()
 
-        <table style="width:100%;" border="1">
-"""
+row = outLines.split("<tr")
+returnArray = []
 
+for elem in row:
+    tempArray = re.findall("\<td[\w]*\>.*\<\/td\>", elem)
+    returnTemp = []
+    for elem_2 in tempArray:
+        returnTemp.append(elem_2[4:-5])
+    if len(returnTemp) != 0:
+        returnArray.append(returnTemp)
 
+print(returnArray)
 
-for element in data:
-    html+= """
-        <tr>
-        <td>"""+element[0]+"""</td>
-        <td>"""+element[1]+"""</td>
-        <td>"""+element[2]+"""</td>
-        </tr>
-
-    """
+#print(re.findall("\<tr[\w]*\> [\w]* \</tr\>", outLines))
 
 
 
-html += """
-        </table>
-    </body>
-</html>
-"""
 
-out.write(html)
-out.close()
